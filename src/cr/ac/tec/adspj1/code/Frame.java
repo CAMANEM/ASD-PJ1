a819abe5acaff3797f7b5f05ccb1a294b73b043e
Fernando <fercoche369@gmail.com>
2020-10-22 02:51:52 -0600

    Final base window and frame

--- src/cr/ac/tec/adspj1/code/Frame.java
+++ src/cr/ac/tec/adspj1/code/Frame.java
@@ -2,6 +2,8 @@ package cr.ac.tec.adspj1.code;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Frame {
     public static void main(String[] args) {
@@ -25,6 +27,41 @@ class myFrame extends JFrame{
 
         Image myIcon = myWindow.getImage("src/cr/ac/tec/adspj1/img/CR.png");
         setIconImage(myIcon);
+
+        canvas myCanvas = new canvas();
+        add(myCanvas);
+    }
+}
+
+class canvas extends JPanel implements ActionListener {
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawString("Default Data", 125, 125);
+
+    }
+    JButton button1 = new JButton("Player 1");
+    JButton button2 = new JButton("Player 2");
+
+    public canvas(){
+        add(button1);
+        add(button2);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object selection = e.getSource();
+
+        if(selection == button1){
+            setBackground(Color.DARK_GRAY);
+            System.out.println("Player 1 was pressed ");
+        }else{
+            setBackground(Color.green);
+            System.out.println("Player 2 was pressed");
+        }
+
+
+    }
 }
